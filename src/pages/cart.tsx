75d953a6724311350331900258bf7e4e5dfbf521
Lamm1234 <Lamm1234@users.noreply.github.com>
2026-08-22 02:11:55 +0700

    Fix useState error in cart.tsx

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AnimatedNavbar } from "@/components/animated-navbar";
 import { AnimatedFooter } from "@/components/animated-footer";
